574ad18ff9a254a2e14fd48e033943c803c8901f
GstavRocha <rocha.gustavo@escolar.ifrn.edu.br>
2024-02-14 06:23:53 -0300

    update

--- Api_ts/Request.js
+++ Api_ts/Request.js
@@ -1,10 +1,20 @@
-var axios = require('axios');
-var URL_POKEMON = "https://pokeapi.co/api/v2/pokemon/35";
-function getName(nom) {
-    axios.get(URL_POKEMON)
+"use strict";
+const axios = require('axios');
+const URL_POKEMON = "https://pokeapi.co/api/v2/pokemon/";
+function getName(nom, ind) {
+    axios.get(URL_POKEMON + ind)
         .then(function (response) {
-        var data = response.data.abilities;
-        console.log(data[0].ability);
+        const data = response.data.abilities;
+        console.log(data[ind].ability.name);
     });
 }
-getName(URL_POKEMON);
+// function getPokeUrl(param: string): any{
+//     axios.get(URL_POKEMON)
+//         .then(function (response: any):any{
+//             const data = response.data.abilities
+//             console.log(data[0].ability.url)
+//             return data[0].ability.url
+//         })
+// }
+getName(URL_POKEMON, 1);
+// getPokeUrl(URL_POKEMON)
--- Api_ts/Request.ts
+++ Api_ts/Request.ts
@@ -1,13 +1,22 @@
 const axios = require('axios');
 
-const URL_POKEMON: string = "https://pokeapi.co/api/v2/pokemon/35";
+const URL_POKEMON: string = "https://pokeapi.co/api/v2/pokemon/";
 
-function getName(nom: string){
-    axios.get(URL_POKEMON)
+function getName(nom: string, ind: number): any{
+    axios.get(URL_POKEMON+ind)
         .then(function (response: any){
             const data = response.data.abilities
-            console.log(data[0].ability)
+            console.log(data[ind].ability.name)
         })
 
 }
-getName(URL_POKEMON);
+// function getPokeUrl(param: string): any{
+//     axios.get(URL_POKEMON)
+//         .then(function (response: any):any{
+//             const data = response.data.abilities
+//             console.log(data[0].ability.url)
+//             return data[0].ability.url
+//         })
+// }
+getName(URL_POKEMON, 1)
+// getPokeUrl(URL_POKEMON)
